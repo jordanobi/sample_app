a7593b8e1e178d241341b52f64c4aeca36b42691
Jordan Obi <jordanobi3@gmail.com>
2014-08-17 22:06:28 -0400

    adds a contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ SampleApp::Application.routes.draw do
                           # to the home action in the StaticPages controller
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
   describe "Home page" do
+
+  	
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -11,7 +14,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -24,7 +27,7 @@ describe "Static pages" do
 
   it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    	expect(page).to have_title("#{base_title} | Help")
     end
 
  end
@@ -32,14 +35,20 @@ describe "Static pages" do
  describe "About page" do
  	it "should have the content 'About'" do
  		visit '/static_pages/about'
- 		expect(page).to have_content('About Us')
+ 		expect(page).to have_content("About Us")
  	end
 
  	it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    	expect(page).to have_title("#{base_title} | About Us")
     end
 
  end
 
+ describe "Contact page" do
+ 	it "should have the title 'Ruby on Rails Tutorial Sample App | Contact'" do
+ 		visit '/static_pages/contact'
+ 		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+ 	end
+ end
 end
